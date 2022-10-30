0d24bbae80d7166427cf364333283bf9394433fe
Bogdan <bogdan.holbora@gmail.com>
2022-10-30 00:57:14 -0700

    Add storage container

--- init.sh
+++ init.sh
@@ -8,8 +8,8 @@ testApplicationRegistrationObjectId=$(echo $testApplicationRegistrationDetails |
 testApplicationRegistrationAppId=$(echo $testApplicationRegistrationDetails | jq -r '.appId')
 
 az ad app federated-credential create \
-  --id $testApplicationRegistrationObjectId \
-  --parameters "{\"name\":\"toy-website-end-to-end-test\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Test\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+   --id $testApplicationRegistrationObjectId \
+   --parameters "{\"name\":\"toy-website-end-to-end-test\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Test\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
 az ad app federated-credential create \
    --id $testApplicationRegistrationObjectId \
@@ -27,17 +27,13 @@ az ad app federated-credential create \
    --id $productionApplicationRegistrationObjectId \
    --parameters "{\"name\":\"toy-website-end-to-end-production-branch\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
-productionApplicationRegistrationDetails=$(az ad app create --display-name 'toy-website-end-to-end-production')
-productionApplicationRegistrationObjectId=$(echo $productionApplicationRegistrationDetails | jq -r '.id')
-productionApplicationRegistrationAppId=$(echo $productionApplicationRegistrationDetails | jq -r '.appId')
+testResourceGroupResourceId=$(az group create --name ToyWebsiteTest --location eastus --query id --output tsv)
 
-az ad app federated-credential create \
-   --id $productionApplicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-end-to-end-production\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Production\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
-
-az ad app federated-credential create \
-   --id $productionApplicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-end-to-end-production-branch\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+az ad sp create --id $testApplicationRegistrationObjectId
+az role assignment create \
+   --assignee $testApplicationRegistrationAppId \
+   --role Contributor \
+   --scope $testResourceGroupResourceId
 
 productionResourceGroupResourceId=$(az group create --name ToyWebsiteProduction --location eastus --query id --output tsv)
 
